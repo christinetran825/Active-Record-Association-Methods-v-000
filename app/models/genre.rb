15dd5ebe8ca61f5746e144536826e926b576377f
Christine Tran <christinetran825@gmail.com>
2017-10-12 05:13:11 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists_all.names
+    self.artists.names
   end
 end
